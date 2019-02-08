d182da6b596f03a245d5ff8a99f25ebb4f9ea8f1
Samuele Cancellieri <32717860+Zanathos113@users.noreply.github.com>
2019-02-08 11:41:12 +0100

    nuovi file e rimossi vecchi

--- sourceCode/Python_Scripts/Enrichment/bcf_query.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-VCFFILES=$1*
-for f in $VCFFILES
-do
-	IFS='.' read -r -a array <<< "$f"
-	echo "Parsing: " ${array[1]}
-	bcftools query -f '%POS %REF %ALT %QUAL\n' $f > ${array[1]}.alt
-done
