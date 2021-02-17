cb1a60731277f081c685d822a416d5deaaa4268c
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-02-17 16:32:22 +0100

    update bcfquery to new format

--- sourceCode/Python_Scripts/Enrichment/bcf_query.sh
+++ sourceCode/Python_Scripts/Enrichment/bcf_query.sh
@@ -5,7 +5,7 @@
 #$2 chr name
 
 echo "Parsing: " $2
-#query VCF file and exctracting position, ref and alt variant and GT*SAMPLEs, filtering out any line with FILTER != PASS
-# bcftools query  -f '%POS %REF\t%ALT[\t%SAMPLES]\n' $1 > $2.alt
-bcftools view -h $1 | fgrep -v '##' > $2.alt
+#query VCF file and exctracting position, ref and alt variant, AF and GT*SAMPLEs, filtering out any line with FILTER != PASS
+
+bcftools view -h $1 | fgrep -v '##' | cut -f1,6,7,8 --complement | sed 's/FORMAT/AF/' > $2.alt
 bcftools query -i 'FILTER="PASS"' -f '%POS\t%ID\t%REF\t%ALT\t%AF[\t%GT]\n' $1 | sed 's/\//|/g' >> $2.alt
