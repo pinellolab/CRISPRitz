1890397825e09ce456007cf37909a8e585fdd9e0
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-04-29 11:13:49 +0200

    local test

--- crispritz.py
+++ crispritz.py
@@ -17,9 +17,9 @@ origin_path = os.path.dirname(os.path.realpath(__file__))
 # conda path
 conda_path = "opt/crispritz/"
 # path corrected to use with conda
-corrected_origin_path = origin_path[:-3]+conda_path
-
-# corrected_origin_path = origin_path+'/sourceCode/' #for quick local tests
+# corrected_origin_path = origin_path[:-3]+conda_path
+#for quick local tests
+corrected_origin_path = origin_path+'/sourceCode/' 
 
 
 def checkExistance(f_path, element):
