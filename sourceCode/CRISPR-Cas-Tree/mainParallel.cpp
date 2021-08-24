c14af8016ac099fbf2e695eedaf5d2db28180456
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-08-24 18:10:17 +0200

    removed print for debug

--- sourceCode/CRISPR-Cas-Tree/mainParallel.cpp
+++ sourceCode/CRISPR-Cas-Tree/mainParallel.cpp
@@ -581,7 +581,7 @@ int main(int argc, char **argv)
 	pamIndices = searchPAMonGenome(pamRNA, pamlen, chrSeq, pamlimit, pam_at_start, max_bulges);
 	// cout << "faccio search\n";
 
-	cout << "lunghezza degli indices " << pamIndices.size() << endl;
+	// cout << "lunghezza degli indices " << pamIndices.size() << endl;
 	//elimino gli indici duplicati dovuti a combinazioni di PAM con lo stesso pattern (IUPAC)
 	// __gnu_parallel::sort(pamIndices.begin(), pamIndices.end());
 	// pamIndices.erase(unique(pamIndices.begin(), pamIndices.end()), pamIndices.end());
