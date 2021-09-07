dc50e88cf18ecdaedc597e14f042b6c750c1a32e
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-09-07 12:36:58 +0200

    test new allocation into char* vector

--- sourceCode/CRISPR-Cas-Tree/searchOnTST.cpp
+++ sourceCode/CRISPR-Cas-Tree/searchOnTST.cpp
@@ -962,9 +962,10 @@ int main(int argc, char **argv)
 		}
 
 		char *writable = new char[iguide.size() + 1];
-		std::copy(iguide.begin(), iguide.end(), writable);
 		writable[iguide.size()] = '\0'; //terminating zero for char*
 		guideRNA.push_back(writable);
+		std::copy(iguide.begin(), iguide.end(), guideRNA[numGuide]);
+
 		delete[] writable;
 		// char *temp_char_guide = (char *)malloc((pamlen - pamlimit + 1) * sizeof(char));
 		// // strcpy(temp_char_guide, iguide.c_str());
