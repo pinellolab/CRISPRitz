e61c9ecea3b4a3c5083588f6505f9d9207d36923
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2021-09-07 13:05:42 +0200

    reset to original

--- sourceCode/CRISPR-Cas-Tree/searchOnTST.cpp
+++ sourceCode/CRISPR-Cas-Tree/searchOnTST.cpp
@@ -962,22 +962,6 @@ int main(int argc, char **argv)
 			reverse(iguide.begin(), iguide.end());
 		}
 
-		// guideRNA.push_back(iguide);
-
-		// char *writable = new char[iguide.size() + 1];
-		// writable[iguide.size()] = '\0'; //terminating zero for char*
-		// guideRNA.push_back(writable);
-		// std::copy(iguide.begin(), iguide.end(), guideRNA[numGuide]);
-
-		// guideRNA.push_back(iguide.c_str());
-
-		// delete[] writable;
-		// char *temp_char_guide = (char *)malloc((pamlen - pamlimit + 1) * sizeof(char));
-		// // strcpy(temp_char_guide, iguide.c_str());
-		// guideRNA.push_back(temp_char_guide);
-		// copy(iguide.begin(), iguide.end(), guideRNA[numGuide]);
-		// // use of pw
-		// delete[] temp_char_guide;
 		guideRNA.push_back((char *)malloc((pamlen - pamlimit) * sizeof(char)));
 		copy(iguide.begin(), iguide.end(), guideRNA[numGuide]); // save Guide
 		guideRNA[numGuide][pamlen - pamlimit + 1] = '\0';
